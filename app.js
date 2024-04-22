08d8dc93bef2c2c2b9231393267358a94d38e5c9
Joe Milburn <jrmilburn@outlook.com>
2024-04-22 19:36:43 +1000

    refactor code

--- app.js
+++ app.js
@@ -1,40 +1,85 @@
-const bookList = document.querySelector('.book-list');
-const addBtn = document.querySelector('#add');
-const refreshBtn = document.querySelector('#refresh');
-const form = document.querySelector('form');
-const submitBtn = document.querySelector('#submit');
-const body = document.querySelector('body');
+const dialog = document.querySelector("dialog");
+const showModal = document.querySelector(".add");
+const closeModal = document.querySelector("dialog .close");
+const mainSection = document.querySelector("main")
 
-const myLibrary = ['Diary of a CEO', 'Thinking Fast and Slow', 'the art of war'];
+showModal.addEventListener("click", () => {
+  dialog.showModal();
+});
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+closeModal.addEventListener("click", (e) => {
+  e.preventDefault();
+  dialog.close();
+});
+
+//--------------------------------------------------------------
+
+const getTitle = document.querySelector("#title");
+const getAuthor = document.querySelector("#author");
+const getPages = document.querySelector("#pages");
+const getStatus = document.querySelector("#status");
+const submitData = document.querySelector("form");
+
+let myLibrary = [];
+
+const addBookToLibrary = () => {
+  myLibrary.push({
+    title: title = getTitle.value,
+    author: author = getAuthor.value,
+    pages: pages = getPages.value,
+    stats: 'checked'
+  })
 }
 
-function addBookToLibrary(book) {
-    let newBook = prompt('What is the new book?');
-    myLibrary.push(book);
-}
-
-function displayBooks(array){
-    for (let i = 0; i < array.length; i++){
-        
-        let book = document.createElement('p');
-        book.textContent = array[i];
-        book.style.cssText = 'border: 1px solid black; border-radius: 1rem; width: 200px; height: auto; padding: 1rem;'
-        bookList.appendChild(book);
+const createBookElement = (elem, content, spans, spanTxt) => {
+  const element = document.createElement(elem);
+  element.textContent = content;
+  const childElem = document.createElement(spans);
+    if (spans === "input") {
+        childElem.setAttribute("type", "checkbox");
     }
-    
+    else {
+      childElem.textContent = spanTxt;
+    }
+  element.appendChild(childElem);
+  return element
 }
 
-refreshBtn.addEventListener('click', () => {
-    displayBooks(myLibrary);
+const createBookDiv = (book) => {
+  const insertBook = document.createElement("div");
+        insertBook.setAttribute("class", "card");
+        const removeBtn = document.createElement("button");
+        removeBtn.setAttribute("class", "delete")
+        const removeIcon = document.createElement("p");
+        removeIcon.textContent = "X";
+        removeBtn.appendChild(removeIcon);
+        insertBook.appendChild(removeBtn);
+        insertBook.appendChild(createBookElement('p', "Title: ", 'span', book.title));
+        insertBook.appendChild(createBookElement('p', "Author: ", 'span', book.author));
+        insertBook.appendChild(createBookElement('p', "Pages: ", 'span', book.pages));
+        insertBook.appendChild(createBookElement('label', "Finished Reading: ", 'input', book.stats));
+        mainSection.appendChild(insertBook);
+}
+
+const displayBook = () => {
+  myLibrary.map((book) => {
+    createBookDiv(book)
+  })
+  myLibrary = [];
+}
+
+submitData.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addBookToLibrary();
+  displayBook();
+  const deleteItem = document.querySelectorAll(".delete");
+  deleteItem.forEach((button) => {
+    button.addEventListener('click', (e) => {
+      e.target.closest('div.card').remove();
+    })
+  })
+  dialog.close();
+  submitData.reset();
 })
 
-addBtn.addEventListener('click', ()=> {
-    form.classList.add('active');
-    body.classList.add('inactive');
-});
+displayBook();
